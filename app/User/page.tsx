520266ed3b5f0543037c3e0cf298ac27553fbd67
siburianbasrunki <basrunkisiburian@gmail.com>
2023-12-02 17:14:08 +0700

    coba deploy

--- app/User/page.tsx
+++ app/User/page.tsx
@@ -89,10 +89,6 @@ const ProfilePage: NextPage = () => {
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                       <span>Gambar dari terumbu karang  {donasi.terumbuKarang.nama} akan diupdate setiap bulan</span>
                     </div>
-                  <div
-                    key={donasi.id}
-                    className="bg-base-200 mb-4 p-4 sm:p-6 lg:p-8"
-                  >
                     <div className="flex flex-col md:flex-row">
                       <Image
                         src={donasi.gambar}
