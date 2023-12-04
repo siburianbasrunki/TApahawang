d2430df4d02ca0bc024168ec70b6fdd57ee7e5b2
siburianbasrunki <basrunkisiburian@gmail.com>
2023-12-04 11:47:01 +0700

    cobalagi

--- app/components/ListHotels.tsx
+++ app/components/ListHotels.tsx
@@ -1,16 +1,33 @@
+"use client"
 import React from "react";
 import iconsBest from "../../public/assets/hotels.svg";
 import Image from "next/image";
-import Villaimg from "../../public/assets/villa.png";
+import useSwr from "swr";
 
-const getVilla = async () => {
-  const res = await fetch(process.env.BASE_URL + "/api/villas");
-  const json = await res.json()
-  return json;
+const fetcher = (url:string) => fetch(url).then((res) => res.json());
+
+const useVilla = () => {
+  const { data, error } = useSwr(`/api/villas`, fetcher);
+  // console.log(data);
+  
+  return {
+    villas: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
 };
 
-const ListHotels = async () => {
-  const villas = await getVilla();
+const ListHotels = () => {
+  const { villas, isLoading, isError } = useVilla();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading data</p>;
+  }
+
   return (
     <div className="bg-[#D9D9D9] text-center">
       <div className="p-4">
@@ -21,7 +38,7 @@ const ListHotels = async () => {
         </small>
       </div>
       <div className="listHotels grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        {villas?.villas?.map((villa: any, index: number) => (
+        {villas?.villas?.map((villa:any, index:any) => (
           <div
             key={index}
             className="card card-compact bg-base-100 shadow-xl sm:w-[282px] sm:h-[327px]"
@@ -37,14 +54,13 @@ const ListHotels = async () => {
             </figure>
             <div className="card-body items-center">
               <h2 className="card-title font-bold">{villa.nama}</h2>
-              
-                <p className="text-sm text-gray-500">{villa.deskripsi}</p>
-                <p className="text-lg font-semibold">
-                  {villa.hargaPerMalam.toLocaleString("id-ID", {
-                    style: "currency",
-                    currency: "IDR",
-                  })}
-                </p>
+              <p className="text-sm text-gray-500">{villa.deskripsi}</p>
+              <p className="text-lg font-semibold">
+                {villa.hargaPerMalam.toLocaleString("id-ID", {
+                  style: "currency",
+                  currency: "IDR",
+                })}
+              </p>
             </div>
           </div>
         ))}
